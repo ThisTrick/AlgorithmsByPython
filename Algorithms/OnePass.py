04b360de54a4123a395f6b04597046f8f07473fd
ThisTrickd.storozhyk <way.trickster@gmail.com>
2021-02-27 20:00:48 +0200

    implemented count

--- Algorithms/OnePass.py
+++ Algorithms/OnePass.py
@@ -1,9 +1,13 @@
 
 def count(array: list) -> int:
-    pass
+    ''' Count of numbers from list'''
+    _c = 0
+    for num in array:
+        _c += 1
+    return _c    
 
 def sum(array: list) -> float:
-    ''' Sum nubber from list '''
+    ''' Sum of numbers from list '''
     _s = 0
     for num in array:
         _s += num
