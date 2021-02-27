40cd070210c081c0593fa3b98de33e5fbedbe0e9
ThisTrickd.storozhyk <way.trickster@gmail.com>
2021-02-27 21:16:41 +0200

    factorize_number

--- Testing/NumetricAlgorithmsTests.py
+++ Testing/NumetricAlgorithmsTests.py
@@ -4,6 +4,7 @@ import Algorithms.NumetricAlgorithms as na
 def start_tests():
     is_simple_nubber_17_returnedTrue()
     is_simple_nubber_35_returnedFalse()
+    factorize_number_1024_returned2x10()
 
 def is_simple_nubber_17_returnedTrue():
     #Arrange
@@ -20,4 +21,12 @@ def is_simple_nubber_35_returnedFalse():
     _actual = na.is_simple_nubber(35)
     #Assert
     Assert.equal(__name__, is_simple_nubber_17_returnedTrue.__name__, _actual, _expected)
+
+def factorize_number_1024_returned2x10():
+    #Arrange
+    _expected = [2] * 10
+    #Act
+    _actual = na.factorize_number(1024)
+    #Assert
+    Assert.arrays_equal(__name__, factorize_number_1024_returned2x10.__name__, _actual, _expected)
 
